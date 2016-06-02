3a9d68b0108e817389b0d92d35b4d765ef719880
Josip Delic <delijati@googlemail.com>
2016-06-02 17:00:47 +0200

    Back to development: 2.0.2

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ docs_extras = [
 
 setupkw = dict(
     name='deform2000',
-    version='2.0.1',
+    version='2.0.2.dev0',
     description='Another form generation library',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
